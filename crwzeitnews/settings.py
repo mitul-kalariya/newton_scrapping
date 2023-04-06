a0d864e3695545e808887ecbcd1e663085c05ccf
Mitul Kalariya <mitul.k@simformsolutions.com>
2023-04-06 18:40:19 +0530

    commit for changes

--- crwzeitnews/settings.py
+++ crwzeitnews/settings.py
@@ -26,13 +26,13 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 # TELNETCONSOLE_ENABLED = False
--- crwzeitnews/spiders/a.py
+++ crwzeitnews/spiders/a.py
@@ -1,5 +1,15 @@
 import scrapy
 from scrapy.utils.response import open_in_browser
+import scrapy
+import time
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 class a_spider(scrapy.Spider):
@@ -27,19 +37,45 @@ class a_spider(scrapy.Spider):
         return cookies
 
     def start_requests(self):
-        headers = {'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
-                   'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Linux"', 'upgrade-insecure-requests': '1',
-                   'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-                   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                   'sec-fetch-site': 'same-origin', 'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1',
-                   'sec-fetch-dest': 'document',
-                   'referer': 'https://www.zeit.de/zustimmung?url=https%3A%2F%2Fwww.zeit.de%2Findex',
-                   'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-                   'cookie': 'creid=1761892688200608988; _k5a=61@{"u":[{"uid":"aDvnTTQYuLXfldY7","ts":1680271807},1680361807]}; wt_fa=lv~1680271807791|1695823807791#cv~1|1695823807793#fv~1680271807800|1695823807801#; wt_fa_s=start~1|1711807807802#; _sp_v1_uid=1:384:01848358-9df8-4a28-8eac-ec0ec570d83c; _sp_v1_ss=1:H4sIAAAAAAAAAItWqo5RKimOUbKKRmbkgRgGtbE6MUqpIGZeaU4OkF0CVlBdi1tCKRYAmuD4I1IAAAA%3D; _sp_su=false; wteid_981949533494636=4168027180800266792; wtsid_981949533494636=1; _sp_enable_dfp_personalized_ads=true; consentUUID=67655ceb-2a75-4f5f-aa2b-b81696677577_18; consentDate=2023-03-31T14:10:23.833Z; _sp_v1_data=2:532696:1680271746:0:1:-1:1:0:0:_:-1; _sp_v1_opt=1:login|true:last_id|11:; _sp_v1_csv=; _sp_v1_lt=1:; zonconsent=2023-03-31T14:10:24.018Z; wt_rla=981949533494636%2C2%2C1680271807819'}
+        headers = {}
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        service = Service(executable_path=ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver.get("https://www.zeit.de/index")
+        time.sleep(5)
 
-        cookie_string = 'creid=1761891441443194415; _k5a=61@{"u":[{"uid":"kXb0YZhbZgmWaE12","ts":1680270618},1680360618]}; wt_fa=lv~1680270618944|1695822618944#cv~1|1695822618945#fv~1680270618946|1695822618946#; wt_fa_s=start~1|1711806618947#; wteid_981949533494636=4168027061900569888; wtsid_981949533494636=1; _sp_v1_uid=1:799:ce56a9e8-0b43-499a-adce-177cefeceb10; _sp_v1_ss=1:H4sIAAAAAAAAAItWqo5RKimOUbKKRmbkgRgGtbE6MUqpIGZeaU4OkF0CVlBdi1tCKRYAmuD4I1IAAAA%3D; _sp_su=false; _sp_enable_dfp_personalized_ads=true; consentUUID=1e7fb635-9b0f-4aae-88b8-f5cfee8d6352_18; consentDate=2023-03-31T13:50:28.061Z; _sp_v1_data=2:532696:1680270437:0:1:-1:1:0:0:_:-1; _sp_v1_opt=1:login|true:last_id|11:; _sp_v1_csv=; _sp_v1_lt=1:; zonconsent=2023-03-31T13:50:28.478Z; wt_rla=981949533494636%2C2%2C1680270618956'
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                '//*[@id="main"]/div/article/div/section[2]/div[1]/div')))
+            banner_button = driver.find_element(By.XPATH, '//*[@id="main"]/div/article/div/section[2]/div[1]/div')
+            if element:
+
+                banner_button.click()
+                article = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH,
+                                                    '/html/body/div[3]')))
+                # '//article[@id="js-article"]')))
+                if article:
+                    data = []
+                    for request in driver.requests:
+                        if "https://www.zeit.de/index" in str(request.url) and "cookie:" in str(request.headers):
+                            # breakpoint()
+                            headers |= self.format_headers(str(request.headers))
+                            print(headers)
+                            print(str(request.headers).split("cookie:"))
+
+                    driver.quit()
+        except:
+            pass
+
+
+        self.valid_cookie = headers.get('cookie')
+        # del headers['cookie']
+        cookie_string = self.valid_cookie
         req = scrapy.Request("https://www.zeit.de/gsitemaps/index.xml", headers=headers,
-            cookies=self.parse_cookies(cookie_string), callback=self.parse)
+            cookies=cookie_string, callback=self.parse)
         yield req
 
     def parse(self, response):
@@ -47,3 +83,34 @@ class a_spider(scrapy.Spider):
         raw_cookies = '; '.join([cookie.decode('utf-8') for cookie in response.headers.getlist('Set-Cookie')])
         print('\n\nSET COOKIE: %s\n\n' % raw_cookies)
         open_in_browser(response)
+
+    def format_headers(self,request_headers, sep=': ', strip_cookie=False, strip_cl=True,
+                       strip_headers: list = []) -> dict:
+        """
+        formates a string of headers to a dictionary containing key-value pairs of request headers
+        :param request_headers:
+        :param sep:
+        :param strip_cookie:
+        :param strip_cl:
+        :param strip_headers:
+        :return: -> dictionary
+        """
+        headers_dict = dict()
+        for keyvalue in request_headers.split('\n'):
+            keyvalue = keyvalue.strip()
+            if keyvalue and sep in keyvalue:
+                value = ''
+                key = keyvalue.split(sep)[0]
+                if len(keyvalue.split(sep)) == 1:
+                    value = ''
+                else:
+                    value = keyvalue.split(sep)[1]
+                if value == '\'\'':
+                    value = ''
+                if strip_cookie and key.lower() == 'cookie': continue
+                if strip_cl and key.lower() == 'content-length': continue
+                if key in strip_headers: continue
+                headers_dict[key] = value
+        breakpoint()
+        headers_dict["cookie"] = self.parse_cookies(headers_dict.get("cookie", None))
+        return headers_dict
